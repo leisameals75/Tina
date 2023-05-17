b020d123fa29b672bd5d8eba17f9e71ef0b0f846
leisameals75 <109402874+leisameals75@users.noreply.github.com>
2023-05-17 20:09:57 +0800

    Add files via upload

---
+++ d03.py
@@ -0,0 +1,9 @@
+fen = input("请输入分钟数：")
+xiaoshi = input("请输入小时数：")
+tian = input("请输入天数：")
+fen_miao = float(fen) * 60
+xiaoshi_miao = float(xiaoshi) * 360
+tian_miao = float(tian) * 24 * 60 * 60
+print(str(fen_miao))
+print(str(xiaoshi_miao))
+print(str(tian_miao))
